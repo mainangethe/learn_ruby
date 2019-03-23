8198a9bf35569353d8f94cda76a92cf093652e37
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-03-23 18:12:39 +0300

    Solve Exercise -3
    - Create `add` function that takes two(2) parameters and adds them
    - Create `subtract` function that takes two(2) parameters and adds them
    - Create `sum` function that takes and array and adds each elemeent
    - Create `multiply` function that takes an arary and multiplies each element
    - Create `power` function that takes two(2) parameters and powers them (left to right)
    - Create `factoral` function that takes one(1) parameter and returns it's factoral
    
    JIRA: BEDT-228

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
 #write your code here
+def add number_1, number_2
+  number_1 + number_2
+end
+
+def subtract number_1, number_2
+  number_1 - number_2
+end
+
+def sum array
+  array.reduce(0) { |sum, current_element| sum + current_element }
+end
+
+def multiply array
+  array.reduce(1) { |final, current_element| final * current_element }
+end
+
+def power number, power
+  number ** power
+end
+
+def factoral number
+  if (number == 0 or number == 1)
+    1
+  else
+    number * factoral(number-1)
+  end
+
+end
