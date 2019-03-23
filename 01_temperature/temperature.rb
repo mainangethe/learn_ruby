d98545d2d64d73f3f12a82b9228d2eea1c1d9544
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-03-23 17:32:51 +0300

    Solve Exercise -2
    - Create ftoc function with one(1) parameter `fahrenheit` that converts it to celsius
    - Creates ctof function with one(1) parameter `celsius` that converts it to fahrenheit
    
    JIRA: 228

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+def ftoc fahrenheit
+  (fahrenheit - 32) * 5.0 / 9.0
+end
+
+def ctof celsius
+  (celsius * 9.0 / 5.0) + 32
+  # sprintf( "%0.1f", (celsius * 9) / 5 + 32)
+end
