161d8921ff9d7f8862a3fb182609dd2723ce7d36
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-03-24 10:03:08 +0300

    Solve Exercise -6
    - Create initialize with instance variable `@title` and assign empty string
    - Create general getter method
    - Create custom setter method `#title` which capitalizes every first word and each respective word apart from articles
    
    JIRA: BEDT-228

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+  def initialize
+    @title = ''
+  end
+
+  attr_reader :title
+
+  def title=(sentence)
+    words = sentence.split
+    words.map do |word|
+      if %w(the and over an a in of).include?(word)
+        word
+      else
+        word.capitalize!
+      end
+    end
+    words.first.capitalize!
+    @title = words.join(" ")
+  end
 end
