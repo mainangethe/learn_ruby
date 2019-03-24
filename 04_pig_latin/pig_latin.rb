8e364b51d325c59d5fdc5d760edb5dd553bc5a51
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-03-24 09:23:01 +0300

    Solved Exercise -5
    Created `.translate` function that takes one(1) parameter `sentence` and splits and checks whether if it's a vowel or a (3/2/1)letter consonant and adds a suffix based on the PigLatin rules.
    
    JIRA: BEDT-228

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,30 @@
-#write your code here
+# One I liked - Credits to https://repl.it/@ikaur
+def translate sentence
+  # Let's add our comparators
+  vowels = %w[ a e i o u ]
+  two_letter_consonants = %w[ ch sh qu th br ]
+  three_letter_consonants = %w[thr sch squ]
+
+  words = sentence.split
+  result = []
+
+  #check each word and adjust accordingly
+  words.each do |word|
+    if
+      vowels.include?word[0]
+      result.push word << 'ay'
+    else
+      if three_letter_consonants.include?word[0] + word[1] + word[2]
+        first_three_letters = word.slice!(0,3)
+        result.push word << first_three_letters << 'ay'
+      elsif two_letter_consonants.include?word[0] + word[1]
+        first_two_letters = word.slice!(0,2)
+        result.push word << first_two_letters << 'ay'
+      else
+        consonant = word.slice!(0)
+        result.push word << consonant << 'ay'
+      end
+    end
+  end
+  result.join(' ')
+end
