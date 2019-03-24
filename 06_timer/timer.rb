5042a816cbbd42664ed75749452a902a643084cf
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-03-24 10:32:06 +0300

    Solve Exercise -7
    - Create initialize method and assign `@seconds` to 0
    - Create getter and setter methods for seconds.
    - Create time_string method that converts time into HH:MM:SS
    
    JIRA: BEDT-227

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
   #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  attr_accessor :seconds
+
+  def time_string
+    hours = @seconds / 3600
+    remainder = @seconds % 3600
+    minutes = remainder / 60
+    seconds = remainder % 60
+    sprintf("%02d:%02d:%02d", hours, minutes, seconds)
+  end
 end
