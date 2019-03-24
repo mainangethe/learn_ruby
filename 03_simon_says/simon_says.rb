1d9f66adc506b8b5456948217c9e542505cbcc53
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-03-24 04:51:59 +0300

    Solve Exercise -4
    - Create `echo` function that takes one(1) parameter and returns it
    - Create `shout` function that takes one(1) parameter and returns it in upcase
    - Create `repeat` function that takes two(2) parameter with a default; repeats the first parameter times the second parameter
    - Create `start_of_the_word` function that takes two(2) parameters with a default; returns the word characters per the second parameter
    - Create `first_word` function with one(1) parameter, and returns the first word
    - Create titelize function that takes one(1) parameter and returns with all words minus articles(unless as first words) as capitalized.
    
    JIRA: BEDT-228

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
 #write your code here
+def echo word
+  "#{word}"
+end
+
+def shout word
+  "#{word.upcase}"
+end
+
+def repeat (word, count = 2)
+  ("#{word } "* count).strip
+end
+
+def start_of_word (word, characters = 1)
+  word[0, characters]
+end
+
+def first_word word
+  word[/^\w+/]
+end
+
+def titleize words
+  words = words.split.map do |word|
+    if %w(the and over an a).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
+end
